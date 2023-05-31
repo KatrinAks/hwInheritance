575e2a9bc28868bf09fd0210124c91a5724657fb
Katrin <e_aksenova80@mail.ru>
2023-05-31 21:21:52 +0300

    Task added

---
+++ src/main/java/Epic.java
@@ -0,0 +1,30 @@
+public class Epic extends Task {
+    protected String[] subtasks;
+
+    public Epic(int id, String[] subtasks) {
+        super(id); // вызов родительского конструктора
+        subtasks = subtasks; // заполнение своих полей
+    }
+
+    public String[] getSubtasks() {
+        return subtasks;
+    }
+
+
+    @Override
+    public boolean matches(String query) {
+
+        for (String task : subtasks) {
+            if (task.contains(query)) {
+                return true;
+            }
+        }
+
+            return false;
+
+    }
+}
+
+
+
+
---
+++ src/test/java/TasksTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TasksTest {
+
+    @Test
+    public void shouldSearchSimple() {
+      SimpleTask simpleTask = new SimpleTask(10, "выполнить задание");
+
+        simpleTask.matches("выполнить задание");
+
+
+        Task expected = simpleTask;
+        Task actual = task.matches(false);
+
+        Assertions.assertEquals(expected, actual);
+
+
+    }
+}
